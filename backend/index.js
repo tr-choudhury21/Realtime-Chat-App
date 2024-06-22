5c70caabcfd172bb0d929ac50e33b13c223a7df3
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-06-22 16:07:31 +0530

    define schemas

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const UserRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -8,6 +9,8 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", UserRoutes);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL;
