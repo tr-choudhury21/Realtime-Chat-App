c40cf1055be2780ce8ff7883679637c9718abf0e
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-06-22 11:51:08 +0530

    database connected & server setup

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
+
+
+mongoose.connect(MONGO_URL)
+.then(() => {
+    console.log("Database connected Successfully!!");
+})
+.catch((err) => {
+    console.log(err.message);
+});
+
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Server started on Port ${PORT}`);
+});
