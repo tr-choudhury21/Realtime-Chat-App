4e48e6f3b8b94bc3e0ddbdd950dbbea588963ca9
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-06-30 16:47:28 +0530

    add messages to database

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,4 @@
+module.exports.addMessage = async (req,res,next) => {};
+
+
+module.exports.getAllMessage = async (req,res,next) => {};
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const UserRoutes = require("./routes/userRoutes");
+const MessageRoutes = require("./routes/messageRoutes");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", UserRoutes);
+app.use("/api/messages", MessageRoutes);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL;
