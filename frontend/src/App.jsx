2a0efbb041fbaca6790d0470b7238abaa9d741f4
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-07-07 17:57:14 +0530

    optimize performance by suspense and lazy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, {Suspense, lazy} from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Chat  from './pages/Chat'
-import SetAvatar from './pages/SetAvatar'
+
+const Chat = lazy(() => import("./pages/Chat"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const SetAvatar = lazy(() => import("./pages/SetAvatar"));
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/setAvatar" element={<SetAvatar />} /> 
-        <Route path="/" element={<Chat />} />
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/setAvatar" element={<SetAvatar />} /> 
+          <Route path="/" element={<Chat />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
